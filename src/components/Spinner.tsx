ce004fe33ab3e33d12dc03db843c4e55844912ab
leyeaa <leyeaa@gmail.com>
2026-01-07 05:38:40 -0330

    Upload portfolio

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,21 @@
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+interface Props {
+  loading: string;
+}
+const Spinner = ({ loading }: Props) => {
+  return (
+    <ClipLoader
+      color="#4338ca"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+};
+
+export default Spinner;
